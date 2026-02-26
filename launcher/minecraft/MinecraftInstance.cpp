a32e9fe217673b706befc6547e3b6656033b663c
DioEgizio <83089242+DioEgizio@users.noreply.github.com>
2026-02-26 09:37:03 +0100

    fix: stop overriding language
    Signed-off-by: DioEgizio <83089242+DioEgizio@users.noreply.github.com>

--- launcher/minecraft/MinecraftInstance.cpp
+++ launcher/minecraft/MinecraftInstance.cpp
@@ -568,8 +568,6 @@ QStringList MinecraftInstance::javaArguments()
 {
     QStringList args;
 
-    args << "-Duser.language=en";
-
     // custom args go first. we want to override them if we have our own here.
     args.append(extraArguments());
 
